2e19a2ee0aa44ac5311eac768f81b188569d7f32
Imoh <113641325+9more@users.noreply.github.com>
2026-07-17 18:30:29 +0000

    Containerize ML API with Docker and Gunicorn

---
+++ app-spam/streamlit_app/utils/preprocessing.py
@@ -0,0 +1,21 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def preprocess_text(text):
+
+    doc = nlp(text)
+
+    tokens = [
+        token.lemma_
+        for token in doc
+        if not token.is_punct
+        and not token.is_stop
+    ]
+
+    return " ".join(tokens).lower().strip()
+
+preprocess_text(
+    "I would not recommend this product"
+)
