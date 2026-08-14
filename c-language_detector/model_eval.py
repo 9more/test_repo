79c0b1d2e0c4c5d4c5f996ee18653bbad0457f18
Imoh <imohtranspose@yahoo.com>
2026-08-14 11:40:25 +0100

    .

--- c-language_detector/model_eval.py
+++ c-language_detector/model_eval.py
@@ -1,3 +1,4 @@
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from model_train import best_grid
+
--- c-language_detector/test.py
+++ c-language_detector/test.py
@@ -6,15 +6,27 @@ from model_train import best_grid
 import joblib
 from data_split import data_split
 BASE_PATH=Path(__file__).parent.resolve().parent
-MODEL_PATH=BASE_PATH.joinpath('c-language_detector' 'models')
+MODEL_PATH=BASE_PATH.joinpath('c-language_detector','models')
 MODELS= ['ComplementNB()',
          'ExtraTreesClassifier()',
          'LinearSVC()',
          'LogisticRegression()',
          'SGDClassifier()',
+         'voting.joblib'
          ]
 print(MODEL_PATH)
-
 print(BASE_PATH)
-
+for model in MODELS:
+    if model == 'voting.joblib':
+        model = joblib.load(MODEL_PATH/model)
+        print(type(model))
+        print(model.classes_)
+        print(model.named_estimators_)
+        texts = [
+            "Hello, how are you today?",
+            "Bonjour, comment allez-vous?",
+            "Hola, ¿cómo estás?",
+            "Guten Morgen, wie geht es dir?",
+        ]
+        print(model.predict(texts))
 
